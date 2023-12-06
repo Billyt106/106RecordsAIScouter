790d5b7529b73998e420fe4133cff940d9333b32
Tauseeq Shafqat <142758969+Billyt106@users.noreply.github.com>
2023-12-06 07:19:00 -0700

    Update server.py

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 CORS(app)
 @app.route('/')
 def index():
-    return render_template('templates/index.html')
+    return render_template('/index.html')
 
 @app.route('/backend', methods=['POST'])
 def run_script():
