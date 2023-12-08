4f674cb9b06042b8e757e8040c41f28c97758652
Tauseeq Shafqat <142758969+Billyt106@users.noreply.github.com>
2023-12-07 21:02:15 -0700

    Create __innit__.py

---
+++ __innit__.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, jsonify, render_template
+import threading
+from flask_cors import CORS  # Import CORS
+import os
+import spacy
+from . import backend
+
+app = Flask(__name__)
+CORS(app)
+@app.route('/')
+def index():
+    return render_template('/index.html')
+
+@app.route('/backend', methods=['POST'])
+def run_script():
+    try:
+        data = request.json
+        insta_username = data.get('insta_username')
+        password = data.get('password')
+
+        if not insta_username or not password:
+            return jsonify({"message": "Username or password missing"}), 400
+
+        thread = threading.Thread(target=backend.main, args=(insta_username, password))
+        thread.start()
+        return jsonify({"message": "Script started"}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+        
+if __name__ == '__main__':
+    # Set host to '0.0.0.0' to listen on all public IPs.
+    # Set port to the environment variable PORT, or 5000 if it's not set.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
